cd539e5b88922a295e358123e0cf7cc65219e4b3
Till Witt <mail@tillwitt.de>
2019-05-09 12:14:19 +0200

    ...

--- scripts/step03.sh
+++ scripts/step03.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo = sub source content
+echo = git???
 git -C /drone/custombuild/ submodule update --recursive --remote
+
+
+echo == /ende git
 ls -la /drone/custombuild/sources/meta-java
 ls -la /drone/custombuild/sources/meta-kernel-addons
 
