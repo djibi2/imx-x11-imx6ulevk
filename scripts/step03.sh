4bcc840e8304eba5882484f3e0a880f8fb67d5f0
Till Witt <mail@tillwitt.de>
2019-05-09 10:04:38 +0200

    custom build script

---
+++ scripts/step03.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo ======
+echo = repo config content
+ls -la /drone/custombuild/config/
+
+echo =
+echo = target config content
+ls -la /data/build/conf/
+
+echo =
+echo = repo sources content
+ls -la /drone/custombuild/sources/
+
+echo =
+echo = target sources content
+ls -la /data/sources/
